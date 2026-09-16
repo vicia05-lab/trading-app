2bda15b477e8d007db09984bded8440049e8e04f
vicia05-lab <vicia05@gmail.com>
2026-09-16 09:48:27 -0400

    Use Nitro v3 startup plugin API

--- server/plugins/sleeve-scheduler.ts
+++ server/plugins/sleeve-scheduler.ts
@@ -1,8 +1,14 @@
-import { defineNitroPlugin } from "nitro/runtime";
+import { definePlugin } from "nitro";
 import { restoreSleeveScheduler, stopSleeveScheduler } from "../../src/desk/sleeve-scheduler";
 
-export default defineNitroPlugin(async (nitroApp) => {
-  await restoreSleeveScheduler();
+export default definePlugin((nitroApp) => {
+  const ready = restoreSleeveScheduler().catch((error) => {
+    console.error("[Trading App] Scheduler startup restore failed.", error);
+  });
+
+  nitroApp.hooks.hook("request", async () => {
+    await ready;
+  });
   nitroApp.hooks.hook("close", () => {
     stopSleeveScheduler();
   });
